aae29cd71aa17aef3dd0c493863f6266803c3079
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-08-15 09:30:39 -0300

    creating a InfoGamerJson type, and adding a ovveride toString for better visualization

--- src/main/kotlin/br/com/kauanpecanha/alugames/models/InfoGamerJson.kt
+++ src/main/kotlin/br/com/kauanpecanha/alugames/models/InfoGamerJson.kt
@@ -1,4 +1,12 @@
 package br.com.kauanpecanha.alugames.models
 
-class InfoGamerJson {
+data class InfoGamerJson(
+    val nome: String,
+    val email: String,
+    val dataNascimento: String,
+    val usuario: String
+) {
+    override fun toString(): String {
+        return ("name: $nome | email: $email | bornDate: $dataNascimento | user: $usuario")
+    }
 }
