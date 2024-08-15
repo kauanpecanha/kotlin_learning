814f18dec7b19ada2fd51d83b4af31e5695da06a
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-08-15 09:31:38 -0300

    updating the file to get objects from github api and returning the list of gamers in json format

--- src/main/kotlin/br/com/kauanpecanha/alugames/services/ApiConsume.kt
+++ src/main/kotlin/br/com/kauanpecanha/alugames/services/ApiConsume.kt
@@ -1,6 +1,8 @@
 package br.com.kauanpecanha.alugames.services
 
+import br.com.kauanpecanha.alugames.models.InfoGamerJson
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import org.example.br.com.kauanpecanha.alugames.models.InfoJogo
 import java.net.URI
 import java.net.http.HttpClient
@@ -32,5 +34,35 @@ class ApiConsume {
         val meuInfoJogo = gson.fromJson(json, InfoJogo::class.java)
 
         return meuInfoJogo
+    }
+
+    fun findGamers():List<InfoGamerJson> {
+        // criação de um cliente para fazer requisições na api
+        val client: HttpClient = HttpClient.newHttpClient()
+
+        // criação de uma nova instância de request
+        val request = HttpRequest.newBuilder()
+            // endpoint com o id especificado pelo usuário
+            .uri(URI.create("https://raw.githubusercontent.com/jeniblodev/arquivosJson/main/gamers.json"))
+            // ?
+            .build()
+
+        // variável para armazenamento da resposta obtida
+        val response = client
+            // request
+            .send(request, BodyHandlers.ofString())
+
+        // json com a resposta
+        val json = response.body()
+
+        val gson = Gson()
+
+        // a linha de código abaixo é necessária, uma vez que cria um objeto anônimo do tipo <List<InfoGamerJson>
+        // a necessidade da mesma decorre do fato de, em tempo de execução, o kotlin apaga as informações de tipos
+        // genéricos
+        val gamerType = object: TypeToken<List<InfoGamerJson>>() {}.type
+        val listGamers: List<InfoGamerJson> = gson.fromJson(json, gamerType)
+
+        return listGamers
     }
 }
