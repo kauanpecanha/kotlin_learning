4ecbd8683d10dd3fee394afcb6a307d0f63a4ec2
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-08-16 11:02:10 -0300

    comentando o escopo testJson por ser desnecessario em tal parte do curso

--- src/main/kotlin/br/com/kauanpecanha/alugames/main/TestJson.kt
+++ src/main/kotlin/br/com/kauanpecanha/alugames/main/TestJson.kt
@@ -1,28 +1,20 @@
 package br.com.kauanpecanha.alugames.main
 import br.com.kauanpecanha.alugames.services.ApiConsume
-import br.com.kauanpecanha.alugames.utils.createGamer
 
 fun main() {
 
-    val apiConsumer = ApiConsume()
-
-    // função que retorna a lista de objetos dos gamers, do tipo JSON
-    val gamersListJson = apiConsumer.findGamers()
-
-    // teste do get da api após refactor
-    val game = apiConsumer.findGame(150)
-
-    // conversão da lista do tipo json para o tipo GAMER
-    val gamersListGamer = gamersListJson.map {
-
-        // utilização de extension function para conversão
-        each -> each.createGamer()
-    }
-
-    // impressão do resultado
-    println(gamersListGamer)
-
-    // impressão do resultado do get da api de games após refactor
-    println(game)
+//    val apiConsumer = ApiConsume()
+//
+//    // função que retorna a lista de objetos dos gamers, do tipo JSON
+//    val gamersList = apiConsumer.findGamers()
+//
+//    // teste do get da api após refactor
+//    val gameList = apiConsumer.findGames()
+//
+//    // impressão do resultado
+//    println(gamersList)
+//
+//    // impressão do resultado do get da api de games após refactor
+//    println(gameList)
 
 }
