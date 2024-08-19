a626b5e557888203155c72bcf8319e4c3af8d195
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-08-19 10:26:18 -0300

    adaptação à nova estrutura de classe e print melhorado das informações do aluguel

--- src/main/kotlin/br/com/kauanpecanha/alugames/models/Aluguel.kt
+++ src/main/kotlin/br/com/kauanpecanha/alugames/models/Aluguel.kt
@@ -2,8 +2,19 @@ package br.com.kauanpecanha.alugames.models
 
 import Jogo
 
-data class Aluguel(val gamer: Gamer, val jogo: Jogo) {
+data class Aluguel(
+        val gamer: Gamer,
+        val jogo: Jogo,
+        val periodo: Periodo
+    ) {
+
+    val valor: Double = jogo.preco * periodo.emDias
+
     override fun toString(): String {
-        return "Aluguel do jogo ${jogo.titulo} por ${gamer.name}"
+        return "\n\nINFORMAÇÕES DO ALUGUEL \n\n " +
+                "Nome do jogo: ${jogo.titulo} \n " +
+                "Nome do alugante: ${gamer.name} \n " +
+                "Durante: ${periodo.emDias} dias \n" +
+                " Preço final: R$ ${valor}"
     }
 }
