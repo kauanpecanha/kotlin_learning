2b9821b93a5cce3fe16740d9e05e3d67b4661312
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-08-19 10:55:19 -0300

    adaptação do escopo para sucessivas adições de novas locações de jogos

--- src/main/kotlin/br/com/kauanpecanha/alugames/main/AluguelTeste.kt
+++ src/main/kotlin/br/com/kauanpecanha/alugames/main/AluguelTeste.kt
@@ -9,13 +9,23 @@ fun main() {
     val apiConsumer = ApiConsume()
 
     val selectedGamer = apiConsumer.findGamers()[3]
-    val selectedGame = apiConsumer.findGames()[10]
 
-    println(selectedGamer)
-    println(selectedGame)
+    val selectedGame1 = apiConsumer.findGames()[10]
+    val selectedGame2 = apiConsumer.findGames()[13]
+    val selectedGame3 = apiConsumer.findGames()[2]
 
-    val aluguel: Aluguel = Aluguel(selectedGamer, selectedGame, Periodo(LocalDate.now(), LocalDate.now().plusDays(7)))
+    val periodo1 = Periodo(LocalDate.now(), LocalDate.now().plusDays(7))
+    val periodo2 = Periodo(LocalDate.now(), LocalDate.now().plusDays(3))
+    val periodo3 = Periodo(LocalDate.now(), LocalDate.now().plusDays(10))
 
-    println(aluguel)
+//    val aluguel1: Aluguel = Aluguel(selectedGamer, selectedGame1, periodo1)
+//    val aluguel2: Aluguel = Aluguel(selectedGamer, selectedGame2, periodo2)
+//    val aluguel3: Aluguel = Aluguel(selectedGamer, selectedGame3, periodo3)
+
+    selectedGamer.alugarJogo(selectedGame1, periodo1)
+    selectedGamer.alugarJogo(selectedGame2, periodo2)
+    selectedGamer.alugarJogo(selectedGame3, periodo3)
+
+    println(selectedGamer.rentedGames)
 
 }
