3e19d8f1fa019eb5d9a4182bc4e44d75537fd3d8
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-08-20 09:33:16 -0300

    passagem da abstract class Plano para sealed class

--- src/main/kotlin/br/com/kauanpecanha/alugames/models/Plano.kt
+++ src/main/kotlin/br/com/kauanpecanha/alugames/models/Plano.kt
@@ -1,6 +1,6 @@
 package br.com.kauanpecanha.alugames.models
 
-abstract class Plano(val tipo: String) {
+sealed class Plano(val tipo: String) {
     open fun calculateRent(aluguel: Aluguel): Double {
         return aluguel.jogo.preco * aluguel.periodo.emDias
     }
