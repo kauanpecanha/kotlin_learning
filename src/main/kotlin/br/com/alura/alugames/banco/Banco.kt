ed2927f0d74106b18cd71b06be075f91b077ff5e
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-08-22 10:07:00 -0300

    simplificação do script de conexão com o banco de dados

--- src/main/kotlin/br/com/alura/alugames/banco/Banco.kt
+++ src/main/kotlin/br/com/alura/alugames/banco/Banco.kt
@@ -1,10 +1,11 @@
 package br.com.alura.alugames.banco
 
-import br.com.alura.alugames.modelo.Jogo
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.SQLException
 
+// instância que estabelece a conexão com o banco de dados
+// nota: o object é uma forma de dizer que haverá apenas uma única instância de determinada classe
 object Banco {
     fun obterConexao(): Connection? {
         return try {
@@ -13,45 +14,5 @@ object Banco {
             e.printStackTrace()
             null
         }
-    }
-
-    fun getJogos(): List<Jogo> {
-        val listaJogos = mutableListOf<Jogo>()
-
-        val conexao = this.obterConexao()
-
-        if (conexao != null) {
-            try {
-                // criação de statement para execução do código
-                val statement = conexao.createStatement()
-
-                // comando de pesquisa no banco de dados
-                val resultado = statement.executeQuery("select * from jogos")
-
-                // loop para mapear todos os objetos advindos do banco de dados
-                while(resultado.next()) {
-                    // captura do id do objeto
-                    val id = resultado.getInt("id")
-                    val titulo = resultado.getString("titulo")
-                    val capa = resultado.getString("capa")
-                    val descricao = resultado.getString("descricao")
-                    val preco = resultado.getDouble("preco")
-
-                    // criação do objeto jogo
-                    val jogo = Jogo(titulo, capa, preco, descricao, id)
-
-                    // adição do jogo à lista
-                    listaJogos.add(jogo)
-                }
-
-                // encerramento do statement
-                statement.close()
-            } finally {
-                // encerramento da conexao
-                conexao.close()
-            }
-        }
-
-        return listaJogos
     }
 }
