1d2724c44a65c826622627aa54307da8d4a3f743
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-08-22 10:08:55 -0300

    script de interações com o banco de dados

--- src/main/kotlin/br/com/alura/alugames/principal/TesteBanco.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/TesteBanco.kt
@@ -2,9 +2,23 @@ package br.com.alura.alugames.principal
 
 import br.com.alura.alugames.banco.Banco
 import br.com.alura.alugames.modelo.Jogo
+import br.com.alura.alugames.banco.JogosDAO
 
+// script de interação com os bancos de dados
 fun main() {
 
-    val listaJogos: List<Jogo> = Banco.getJogos()
+    val jogoTheLastOfUs: Jogo = Jogo(
+        "The Last of Us Part I",
+        "https://cdn.cloudflare.steamstatic.com/steam/apps/1888930/header.jpg?t=1686864554",
+        5.99,
+        "Uma aventura pós-apocalíptica de sobrevivência em um mundo infestado por zumbis e facções em conflito."
+    )
+
+    val jogosDAO = JogosDAO()
+
+    jogosDAO.adicionarJogo(jogoTheLastOfUs)
+
+    val listaJogos: List<Jogo> = jogosDAO.getJogos()
+
     println(listaJogos)
 }
