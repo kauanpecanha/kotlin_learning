0c3f5c14e6255442018106737c4ef257da14eccb
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-08-30 19:27:59 -0300

    starting the hibernate implementation

--- src/main/kotlin/br/com/alura/alugames/banco/Banco.kt
+++ src/main/kotlin/br/com/alura/alugames/banco/Banco.kt
@@ -1,18 +1,23 @@
 package br.com.alura.alugames.banco
 
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
+import javax.persistence.EntityManager
+import javax.persistence.EntityManagerFactory
+import javax.persistence.Persistence
 
 // instância que estabelece a conexão com o banco de dados
 // nota: o object é uma forma de dizer que haverá apenas uma única instância de determinada classe
 object Banco {
-    fun obterConexao(): Connection? {
-        return try {
-            DriverManager.getConnection("jdbc:mysql://localhost:3306/alugames", "root", "root")
-        } catch (e: SQLException) {
-            e.printStackTrace()
-            null
-        }
+//    fun obterConexao(): Connection? {
+//        return try {
+//            DriverManager.getConnection("jdbc:mysql://localhost:3306/alugames", "root", "root")
+//        } catch (e: SQLException) {
+//            e.printStackTrace()
+//            null
+//        }
+//    }
+
+    fun getEntityManagerFactory(): EntityManager {
+        val factory: EntityManagerFactory = Persistence.createEntityManagerFactory("alugames")
+        return factory.createEntityManager()
     }
 }
