924f6c6b786a64d50b1978f5ca32a82d6d76ebca
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-09-01 11:38:22 -0300

    adaptação do jogosDAO para o uso do hibernate

--- src/main/kotlin/br/com/alura/alugames/banco/JogosDAO.kt
+++ src/main/kotlin/br/com/alura/alugames/banco/JogosDAO.kt
@@ -1,85 +1,21 @@
 package br.com.alura.alugames.banco
 
 import br.com.alura.alugames.modelo.Jogo
+import br.com.alura.alugames.modelo.JogoEntity
+import javax.persistence.EntityManager
 
 // classe que contém todas as operações de bancos de dados com os jogos inclusos. algo semelhante aos controllers do js
-class JogosDAO {
+class JogosDAO(val manager: EntityManager) {
 
     fun getJogos(): List<Jogo> {
-        val manager = Banco.getEntityManagerFactory()
-
-        try {
-            val query = manager.createQuery("FROM Jogo", Jogo::class.java)
-        } finally {
-            manager.close()
-        }
+        val query = manager.createQuery("FROM JogoEntity", JogoEntity::class.java)
+        return query.resultList.map { entity -> Jogo(entity.titulo, entity.capa, entity.preco, entity.descricao, entity.Id) }
     }
 
-//
-//    // função de busca de todos os inclusos. semelhante ao get all
-//    fun getJogos(): List<Jogo> {
-//        val listaJogos = mutableListOf<Jogo>()
-//        val conexao = Banco.obterConexao()
-//
-//        if (conexao != null) {
-//            try {
-//                // criação de statement para execução do código
-//                val statement = conexao.createStatement()
-//                // comando de pesquisa no banco de dados
-//                val resultado = statement.executeQuery("SELECT * FROM JOGOS")
-//
-//                // loop para mapear todos os objetos advindos do banco de dados
-//                while(resultado.next()) {
-//                    // captura do id do objeto
-//                    val id = resultado.getInt("id")
-//                    val titulo = resultado.getString("titulo")
-//                    val capa = resultado.getString("capa")
-//                    val descricao = resultado.getString("descricao")
-//                    val preco = resultado.getDouble("preco")
-//
-//                    // criação do objeto jogo
-//                    val jogo = Jogo(titulo, capa, preco, descricao)
-//                    // adição do jogo à lista
-//                    listaJogos.add(jogo)
-//                }
-//
-//                // encerramento do statement
-//                statement.close()
-//            } finally {
-//                // encerramento da conexao
-//                conexao.close()
-//            }
-//        }
-//
-//        return listaJogos
-//    }
-//
-//    // função de inclusão de um jogo. semelhante ao insertOne
-//    fun adicionarJogo(jogo: Jogo) {
-//        val conexao = Banco.obterConexao()
-//
-//        // string de comando do sql que será executasa somente quanto o statement estiver preparado
-//        val insert = "INSERT INTO JOGOS (TITULO, CAPA, PRECO, DESCRICAO) VALUES (?,?,?,?)"
-//
-//        if (conexao != null) {
-//            try {
-//                // preparação do statement
-//                val statement = conexao.prepareStatement(insert)
-//
-//                // inclusão dos atributos nos respectivos pontos de interrogação
-//                statement.setString(1, jogo.titulo)
-//                statement.setString(2, jogo.capa)
-//                statement.setDouble(3, jogo.preco)
-//                statement.setString(4, jogo.descricao)
-//
-//                // efetuação do update dos campos
-//                statement.executeUpdate()
-//
-//                // fechamento do statement
-//                statement.close()
-//            } finally {
-//                conexao.close()
-//            }
-//        }
-//    }
+    fun adicionarJogo(jogo: Jogo) {
+        val entity = JogoEntity(jogo.titulo, jogo.capa, jogo.preco, jogo.descricao)
+        manager.transaction.begin()
+        manager.persist(entity)
+        manager.transaction.commit()
+    }
 }
