1b81f77982dab2ef314691e13ea1d27b94669644
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-09-01 11:40:01 -0300

    criando jogoEntity para correlação de dados entre banco de dados e objetos

--- src/main/kotlin/br/com/alura/alugames/modelo/JogoEntity.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/JogoEntity.kt
@@ -1,4 +1,16 @@
 package br.com.alura.alugames.modelo
 
-class JogoEntity {
+import javax.persistence.*
+
+@Entity
+@Table(name = "jogos")
+open class JogoEntity(
+    val titulo: String = "Titulo do jogo",
+    val capa: String = "Capa do jogo",
+    val preco: Double = 0.0,
+    val descricao: String? = null,
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val Id: Int = 0
+) {
+
 }
