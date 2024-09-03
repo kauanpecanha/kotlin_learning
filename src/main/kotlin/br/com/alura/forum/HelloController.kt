79eaf672261c3e48780d7e87c1795622a829856a
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-09-03 09:51:08 -0300

    setup do projeto

---
+++ src/main/kotlin/br/com/alura/forum/HelloController.kt
@@ -0,0 +1,21 @@
+package br.com.alura.forum
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+
+// especifica a rota de entrada da classe
+@RequestMapping("/hello")
+
+// classe que será executada quando a rota for acessada
+class helloController {
+    // especificando que o request é do tipo get
+    @GetMapping
+    // função a ser executada
+    fun hello(): String {
+        // retorno da string
+        return "Hello World!"
+    }
+}
