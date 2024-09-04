40331db6cab0920d435770d5bdfbfc56c0ec1078
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-09-04 09:18:46 -0300

    implementando a busca por objetos específicos através de uri params e filtragem

--- src/main/kotlin/br/com/alura/forum/controllers/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controllers/TopicoController.kt
@@ -5,6 +5,7 @@ import br.com.alura.forum.models.Topico
 import br.com.alura.forum.models.Usuario
 import br.com.alura.forum.services.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
@@ -18,5 +19,18 @@ class TopicoController(
     @GetMapping
     fun listar(): List<Topico>{
         return service.listar()
+    }
+
+    /*
+    * a função abaixo faz algumas coisas:
+    *   1 - especifica a rota específica que deseja receber a requisição
+    *   2 - pela rota, consegue receber um parâmetro para seu uso interno
+    *
+    * dessa forma, é possível receber parâmetros através de uris
+    *
+    * */
+    @GetMapping("/{id}")
+    fun buscarPorId(@PathVariable id: Long): Topico {
+        return service.buscarPorId(id)
     }
 }
