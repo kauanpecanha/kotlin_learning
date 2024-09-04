9e7b4e6caeb51fb37964138f4fc4a1fdc6cc15dd
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-09-04 19:18:25 -0300

    estabelecendo o retorno como TopicoView, ou seja, um DTO de saída

---
+++ src/main/kotlin/br/com/alura/forum/dto/TopicoView.kt
@@ -0,0 +1,4 @@
+package br.com.alura.forum.dto
+
+class TopicoView {
+}
--- src/main/kotlin/br/com/alura/forum/services/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/services/TopicoService.kt
@@ -1,9 +1,11 @@
 package br.com.alura.forum.services
 
 import br.com.alura.forum.dto.TopicoDTO
+import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.models.Topico
 import org.springframework.stereotype.Service
 import java.util.*
+import java.util.stream.Collectors
 
 @Service
 class TopicoService(
@@ -13,15 +15,31 @@ class TopicoService(
 ) {
 
     // função de busca por todos os objetos cadastrados
-    fun listar(): List<Topico> {
-        return topicos
+    fun listar(): List<TopicoView> {
+        return topicos.stream().map {
+            each -> TopicoView(
+                id = each.id,
+                titulo = each.titulo,
+                mensagem = each.mensagem,
+                status = each.status,
+                dataCriacao = each.dataCriacao
+            )
+        }.collect(Collectors.toList())
     }
 
     // função de busca de um objeto específico através de filtragem
-    fun buscarPorId(id: Long): Topico {
-        return topicos.stream().filter({
+    fun buscarPorId(id: Long): TopicoView {
+        val topico = topicos.stream().filter {
             each -> each.id == id
-        }).findFirst().get()
+        }.findFirst().get()
+
+        return TopicoView(
+            topico.id,
+            topico.titulo,
+            topico.mensagem,
+            topico.status,
+            topico.dataCriacao
+        )
 
     }
 
