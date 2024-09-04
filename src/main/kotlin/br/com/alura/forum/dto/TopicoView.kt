bdd253416de45bb06e70c45d2b5d640326264b03
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-09-04 19:19:42 -0300

    estabelecendo o dto de saída, ou View

--- src/main/kotlin/br/com/alura/forum/dto/TopicoView.kt
+++ src/main/kotlin/br/com/alura/forum/dto/TopicoView.kt
@@ -1,4 +1,13 @@
 package br.com.alura.forum.dto
 
-class TopicoView {
-}
+import br.com.alura.forum.models.StatusTopico
+import ch.qos.logback.core.status.Status
+import java.time.LocalDateTime
+
+data class TopicoView (
+    val id: Long?,
+    val titulo: String,
+    val mensagem: String,
+    val status: StatusTopico,
+    val dataCriacao: LocalDateTime
+)
