cb7d288f784afd813725d6295c1c42c465cd293b
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-09-04 09:01:14 -0300

    fazendo o primeiro service, responsável por retornar uma lista de tópicos

--- src/main/kotlin/br/com/alura/forum/services/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/services/TopicoService.kt
@@ -1,4 +1,22 @@
 package br.com.alura.forum.services
 
+import br.com.alura.forum.models.Curso
+import br.com.alura.forum.models.Topico
+import br.com.alura.forum.models.Usuario
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
 class TopicoService {
+    fun listar(): List<Topico> {
+
+        val topico = Topico(1,
+            "Dúvida de Kotlin",
+            "Variáveis no Kotlin",
+            curso = Curso(id = 1, nome = "Kotlin", categoria = "Programação"),
+            autor = Usuario(id = 1, "Ana da Silva", "ana@email.com"),
+        )
+
+        return Arrays.asList(topico, topico, topico)
+    }
 }
