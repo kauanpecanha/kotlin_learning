a206fc27437b67fdffea6efb2b120911624bbae3
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-09-05 10:09:03 -0300

    adding validators to api request body parameters

--- src/main/kotlin/br/com/alura/forum/dto/TopicoForm.kt
+++ src/main/kotlin/br/com/alura/forum/dto/TopicoForm.kt
@@ -5,8 +5,8 @@ import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
 data class TopicoForm (
-    @NotEmpty @Size(min = 5, max = 100) val titulo: String,
-    @NotEmpty val mensagem: String,
-    @NotNull val idCurso: Long,
-    @NotNull val idAutor: Long
+    @field: NotEmpty @field: Size(min = 5, max = 100) val titulo: String,
+    @field: NotEmpty val mensagem: String,
+    @field: NotNull val idCurso: Long,
+    @field: NotNull val idAutor: Long
 )
