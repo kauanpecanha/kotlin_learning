f08ff50494abdf3f99fdfff1fd8e2fd6705e6833
Kauan Peçanha <kauanpecanha1@gmail.com>
2024-09-09 09:53:42 -0300

    adaptação da controladora ao uso de banco de dados, fazendo chamadas para as services

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -2,34 +2,22 @@ package br.com.alura.forum.controller
 
 import br.com.alura.forum.dto.AtualizacaoTopicoForm
 import br.com.alura.forum.dto.NovoTopicoForm
-import br.com.alura.forum.dto.TopicoPorCategoriaDto
 import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.service.TopicoService
-import org.springframework.cache.annotation.CacheEvict
-import org.springframework.cache.annotation.Cacheable
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
-import org.springframework.data.domain.Sort
-import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.util.UriComponentsBuilder
 import javax.validation.Valid
-import javax.validation.constraints.NotNull
 
 @RestController
 @RequestMapping("/topicos")
 class TopicoController(private val service: TopicoService) {
 
     @GetMapping
-    @Cacheable("topicos")
-    fun listar(
-            @RequestParam(required = false) nomeCurso: String?,
-            @PageableDefault(size = 5, sort = ["dataCriacao"], direction = Sort.Direction.DESC) paginacao: Pageable
-    ): Page<TopicoView> {
-        return service.listar(nomeCurso, paginacao)
+    fun listar(): List<TopicoView> {
+        return service.listar()
     }
 
     @GetMapping("/{id}")
@@ -38,11 +26,10 @@ class TopicoController(private val service: TopicoService) {
     }
 
     @PostMapping
-    @Transactional
-    @CacheEvict(value = ["topicos"], allEntries = true)
+    @Transactional // tag necessária para indicar alterações seguidas de commits no sql
     fun cadastrar(
-            @RequestBody @Valid form: NovoTopicoForm,
-            uriBuilder: UriComponentsBuilder
+        @RequestBody @Valid form: NovoTopicoForm,
+        uriBuilder: UriComponentsBuilder
     ): ResponseEntity<TopicoView> {
         val topicoView = service.cadastrar(form)
         val uri = uriBuilder.path("/topicos/${topicoView.id}").build().toUri()
@@ -51,7 +38,6 @@ class TopicoController(private val service: TopicoService) {
 
     @PutMapping
     @Transactional
-    @CacheEvict(value = ["topicos"], allEntries = true)
     fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm): ResponseEntity<TopicoView> {
         val topicoView = service.atualizar(form)
         return ResponseEntity.ok(topicoView)
@@ -60,14 +46,8 @@ class TopicoController(private val service: TopicoService) {
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @Transactional
-    @CacheEvict(value = ["topicos"], allEntries = true)
     fun deletar(@PathVariable id: Long) {
         service.deletar(id)
-    }
-
-    @GetMapping("/relatorio")
-    fun relatorio(): List<TopicoPorCategoriaDto> {
-        return service.relatorio()
     }
 
 }
